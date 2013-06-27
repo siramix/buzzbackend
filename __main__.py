29c366e35dc167406d7ac31b6d2deb85083ecc1f
Patrick Reynolds <cpreynolds@gmail.com>
2013-06-27 02:06:38 -0400

    Restructuring and adding documentation.

---
+++ __main__.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+# Library:   buzzbackend
+#
+# Copyright 2013 Siramix Labs
+#
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 ( the "License" );
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+
+import ConfigParser
+import os.path
+import getpass
+import db
+
+
+def get_config():
+    """Read the config object from the user home directory."""
+    home_dir = os.path.expanduser('~')
+    config_path = os.path.join(home_dir, '.buzzbackconfig')
+    config = ConfigParser.ConfigParser()
+    config.read(config_path)
+    if not config.has_section('Credentials'):
+        config.add_section('Credentials')
+    return (config, config_path)
+
+
+def get_credentials(refresh=False):
+    """Get the user credentials from the keyboard or config file."""
+
+    # Get the config object and its path
+    config, config_path = get_config()
+
+    # Get email
+    if config.has_option('Credentials', 'email') and not refresh:
+        email = config.get('Credentials', 'email')
+    else:
+        email = raw_input('Email: ')
+    config.set('Credentials', 'email', email)
+
+    # Get password
+    if config.has_option('Credentials', 'password') and not refresh:
+        password = config.get('Credentials', 'password')
+    else:
+        password = getpass.getpass('Password: ')
+    config.set('Credentials', 'password', password)
+
+    # Save configuration to disk
+    with open(config_path, 'w') as config_file:
+        config.write(config_file)
+
+    return (email, password)
+
+
+def ship_pack_ui(database):
+    """Display the UI for shipping a given pack."""
+    packs = database.get_packs()
+    pack_dictionary = dict(packs)
+    print('Which pack would you like to ship?')
+    for index, pack in packs:
+        print('  %s: %s' % (index, pack))
+    ship_index = raw_input('Which pack would you like to ship? ')
+    ship_name = pack_dictionary[ship_index]
+    return database.ship_pack(ship_name)
+
+
+def login_to_database():
+    """Login to the database and return the db object."""
+    spreadsheet_key = '0Ar6_A4FPzJBPdFlZTEN5REJJYVMtejI1RGkwYW1FX2c'
+
+    # Login. Loop until it works.
+    email, password = get_credentials()
+    database = db.WordDatabase(email, password, spreadsheet_key)
+    login_state = database.login_to_spreadsheet()
+    while not login_state:
+        print('Incorrect Credentials. Please Try Again!')
+        email, password = get_credentials(True)
+        database = db.WordDatabase(email, password, spreadsheet_key)
+        login_state = database.login_to_spreadsheet()
+
+    return database
+
+
+def main():
+    database = login_to_database()
+    print(ship_pack_ui(database))
+
+
+if __name__ == '__main__':
+    main()
---
+++ db.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############################################################################
+#
+# Library:   buzzbackend
+#
+# Copyright 2013 Siramix Labs
+#
+# All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 ( the "License" );
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+import gdata.spreadsheet.service
+import gdata.service
+
+
+class WordDatabase(object):
+    """Class for connecting to the word database."""
+
+    def __init__(self, email, password, spreadsheet_key):
+        """Create an instance of the word database."""
+        super(WordDatabase, self).__init__()
+        self.spreadsheet_key = spreadsheet_key
+        self.email = email
+        self.password = password
+        self.client = None
+
+    def login_to_spreadsheet(self):
+        """Login to the spreadsheet using the stored credentials."""
+        if self.client is None:
+            self.client = gdata.spreadsheet.service.SpreadsheetsService()
+
+        # Catch the error and turn it boolean
+        try:
+            self.client.ClientLogin(self.email, self.password)
+            ret = True
+        except gdata.service.BadAuthentication:
+            ret = False
+        finally:
+            return ret
+
+    def get_worksheet_id(self, worksheet_id):
+        """Get the unique id of a the worksheet by index."""
+        ws_feed = self.client.GetWorksheetsFeed(self.spreadsheet_key)
+        wksht_id_parts = ws_feed.entry[worksheet_id].id.text.split('/')
+        return wksht_id_parts[len(wksht_id_parts) - 1]
+
+    def get_packs(self):
+        """Get a tuple consisting of pack ids and names."""
+        wksht_id = self.get_worksheet_id(1)
+        query = gdata.spreadsheet.service.CellQuery()
+        query.min_col = '1'
+        query.max_col = '2'
+        query.min_row = '4'
+        cell_feed = self.client.GetCellsFeed(self.spreadsheet_key,
+                                             wksht_id,
+                                             query=query)
+        data = [entry.content.text for entry in cell_feed.entry]
+        return zip(data[0::2], data[1::2])
+
+    def determine_start_id(self):
+        """Get the next unique ID in sequence."""
+        wksht_id = self.get_worksheet_id(0)
+        query = gdata.spreadsheet.service.CellQuery()
+        query.min_col = '1'
+        query.max_col = '1'
+        query.min_row = '2'
+        cell_feed = self.client.GetCellsFeed(self.spreadsheet_key,
+                                             wksht_id,
+                                             query=query)
+
+        # Determine the max id and add one to it
+        start_id = max([int(entry.content.text) for entry in cell_feed.entry])
+        start_id += 1
+
+        return start_id
+
+    def ship_pack(self, name):
+        """Output the designated pack."""
+        wksht_id = self.get_worksheet_id(0)
+        query = gdata.spreadsheet.service.CellQuery()
+        query.min_col = '2'
+        query.max_col = '2'
+        query.min_row = '2'
+        cell_feed = self.client.GetCellsFeed(self.spreadsheet_key,
+                                             wksht_id,
+                                             query=query)
+        cell_rows = set()
+        for entry in cell_feed.entry:
+            if entry.content.text == name:
+                cell_rows.add(int(entry.cell.row))
+        query.min_row = str(min(cell_rows))
+        query.max_row = str(max(cell_rows))
+        query.min_col = '1'
+        query.max_col = '10'
+        cell_feed = self.client.GetCellsFeed(self.spreadsheet_key,
+                                             wksht_id,
+                                             query=query)
+        count = 1
+        cards = list()
+        cur_card = dict()
+        bad_words = list()
+        for entry in cell_feed.entry:
+            if count % 10 == 0:
+                cur_card['badwords'] = ",".join(bad_words)
+                if entry.content.text == 'Final' or entry.content.text == 'Shipped':
+                    cards.append(cur_card)
+                else:
+                    print('Warning: "{0}" is not ready'.format(cur_card['title']))
+                cur_card = dict()
+                bad_words = list()
+            elif count % 10 == 1:
+                cur_card['_id'] = entry.content.text
+            elif count % 10 == 3:
+                cur_card['title'] = entry.content.text
+            elif count % 10 >= 4 and count % 10 <= 8:
+                bad_words.append(entry.content.text)
+            count += 1
+        return cards
